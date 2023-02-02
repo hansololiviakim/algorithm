fe22a7c3cf03701cce12b8520f7f16cb8f727442
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-02 10:29:08 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/README.md
+++ 프로그래머스/lv0/120830. 양꼬치/README.md
@@ -1,10 +1,10 @@
 # [level 0] 양꼬치 - 120830 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120830) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120830?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.03 ms
+메모리: 33.5 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,12 +1,5 @@
 function solution(n, k) {
     let answer = 0;
-    let service = 0;
-    
-    if(n >= 10){
-        service = Math.floor(n / 10);
-    }
-    
-    answer = (n * 12000) + (k * 2000) - (service * 2000);
-    
+    answer = (n * 12000) + (k * 2000) - (Math.floor(n / 10) * 2000);
     return answer;
 }
