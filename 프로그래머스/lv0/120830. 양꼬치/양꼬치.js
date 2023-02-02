5084575465bf1c4a3e4fbdab0201d8db866225c5
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-02 10:31:32 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,5 +1,5 @@
 function solution(n, k) {
     let answer = 0;
-    answer = (n * 12000) + (k * 2000) - (Math.floor(n / 10) * 2000);
+    answer = (n * 12000) + ((k - Math.floor(n / 10)) * 2000);
     return answer;
 }
