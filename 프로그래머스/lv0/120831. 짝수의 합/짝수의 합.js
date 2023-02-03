7ca590aa89bc1f850a5be43a3577d34587071b76
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-04 02:32:26 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    var answer = 0;
-    for(i = 0; i <= n; i++){
-        if(i % 2 == 0){
-            answer += i;
-        }
+    let answer = 0;
+    for(i = 0; i <= n; i += 2){
+        answer += i;
     }
     return answer;
 }
