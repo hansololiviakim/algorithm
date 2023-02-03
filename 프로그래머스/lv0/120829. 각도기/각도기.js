3e01357425171a087df53533830fb064c44fee76
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-04 02:29:39 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,9 +1,9 @@
 function solution(angle) {
-    var answer = 0;
+    let answer = 0;
     
     if(angle < 90) {
         answer = 1;
-    }else if(angle == 90) {
+    }else if(angle === 90) {
         answer = 2;
     }else if(angle < 180) {
         answer = 3;
