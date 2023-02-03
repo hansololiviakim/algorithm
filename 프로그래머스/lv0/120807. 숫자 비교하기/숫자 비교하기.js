6bf5bcc3d15b58ecba9f32e6ee54131bb2e2d8bc
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-04 02:28:10 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,9 +1,4 @@
 function solution(num1, num2) {
-    var answer = 0;
-    if(num1 !== num2){
-        answer = -1;
-    }else{
-        answer = 1;
-    }
+    let answer = num1 !== num2 ? -1 : 1;
     return answer;
 }
