0c5d64630ccfffb447995697d69061ef0ae9a2bf
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-13 17:56:54 +0900

    [level 1] Title: 최소직사각형, Time: 7.27 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,27 +1,18 @@
 function solution(sizes) {
+    let longer = [];
+    let shorter = [];
     
-    for(i = 0; i < sizes.length; i++) {
-        if(sizes[i][0] > sizes[i][1]) {
-            sizeComparison(sizes[i], 0);
+    for(i in sizes) {
+        if(sizes[i][0] > sizes[i][1]){
+            longer.push(sizes[i][0]);
+            shorter.push(sizes[i][1]);
         }else{
-            sizeComparison(sizes[i], 1);
+             longer.push(sizes[i][1]);
+             shorter.push(sizes[i][0]);
         }
     }
     
-    // return biggerSideMax * smallerSideMax;
-}
-
-
-function sizeComparison(size, num){
-    let biggerSideMax = 0;
-    let smallerSideMax = 0;
-    
-    // console.log(num)
-
-    if(size[num] > biggerSideMax) {
-        biggerSideMax = size[num];
-    }
-    if(size[num] > smallerSideMax) {
-        smallerSideMax = size[num];
-    }
+    biggerSideMax = Math.max(...longer);
+    smallerSideMax = Math.max(...shorter);
+    return biggerSideMax * smallerSideMax;
 }
