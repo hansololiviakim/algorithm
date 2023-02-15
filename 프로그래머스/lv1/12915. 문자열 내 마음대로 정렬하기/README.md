f417baf0cc082a13e3dff6ccb0361657040e7f16
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-15 19:43:41 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.16 ms
+메모리: 33.5 MB, 시간: 0.15 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,8 +1,8 @@
 function solution(strings, n) {
     let answer = strings.sort((a, b) => {
-        if(a.at(n) > b.at(n)) return 1
-        if(a.at(n) == b.at(n)) return (a > b ? 1 : -1)
-        if(a.at(n) < b.at(n)) return -1
+        if(a[n] > b[n]) return 1;
+        if(a[n] === b[n]) return (a > b ? 1 : -1)
+        if(a[n] < b[n]) return -1;
     })
     return answer;
 }
