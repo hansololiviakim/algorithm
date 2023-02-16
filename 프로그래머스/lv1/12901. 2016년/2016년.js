708a3e8fec63b1b7654bc92a1ad1d4b2e54652f6
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-16 12:02:39 +0900

    [level 1] Title: 2016년, Time: 0.12 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,7 +1,6 @@
 function solution(a, b) {
     const DATE = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
-    let str = `2016-${a}-${b}`;
-    let inputDay = new Date(str);
+    let inputDay = new Date(`2016-${a}-${b}`);
     let getDay = inputDay.getDay();
     return DATE[getDay]
 }
