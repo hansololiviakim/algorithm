e8db794fa9fe1568805bb885e345219f7a8201c9
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-17 22:53:33 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    if(num === 1) return 0;
+    for(let i = 1; i <= 500; i ++) {
+        num % 2 ? num = num * 3 + 1 : num = num / 2;
+        if(num === 1) return i;
+        else if(num !== 1 && i === 500) return -1;
+    }
+
+}
