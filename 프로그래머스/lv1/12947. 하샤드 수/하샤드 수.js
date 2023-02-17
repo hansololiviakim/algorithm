6effa55cb83e12b7968433d582feb689e5b6e845
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-17 23:14:36 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,8 +1,6 @@
-// x의 자릿수의 합으로 x가 나누어져야 합니다
-// 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다.
 function solution(x) {
     let str = String(x).split('');
     let sum = 0;
     for(let i = 0; i < str.length; i++) sum += Number(str[i]);
-    return x % sum === 0 ? true : false;    
+    return x % sum === 0;    
 }
