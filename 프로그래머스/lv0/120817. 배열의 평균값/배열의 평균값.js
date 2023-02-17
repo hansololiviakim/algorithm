af2539f3c6a71b378966de1ba6c4ec5d2f779bbe
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-18 00:11:42 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,9 +1,5 @@
 function solution(numbers) {
-    let sum = 0;    
-    if(numbers.length % 2) {
-        sum = ((numbers[1] + numbers[numbers.length - 1]) * (numbers.length - 1) / 2 + numbers[0]);
-    } else {
-        sum = (numbers[0] + numbers[numbers.length - 1]) * (numbers.length / 2);
-    }
+    let sum = 0;
+    for(let i of numbers) sum += i;
     return sum / numbers.length;
 }
