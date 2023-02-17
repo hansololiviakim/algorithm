385395dc80200010eed1b3ec713cb3b53cea0b6f
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-17 16:02:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,6 @@
 function solution(n){
     let str = String(n);
     let answer = 0;
-    for(let i = 0; i < str.length; i++) answer += str[i] * 1
-    return answer
+    for(let i = 0; i < str.length; i++) answer += Number(str[i]);
+    return answer;
 }
