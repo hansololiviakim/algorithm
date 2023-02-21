399d93b5e7fd320163cb6471656b56dea8f7eec6
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-02-21 14:52:49 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,5 +1,5 @@
 function solution(n) {
     let arr = [];
-    for(let i = 0; i <= n; i++) if(n % i === 0) arr.push(n / i);
+    for(let i = 1; i <= n; i++) if(n % i === 0) arr.push(n / i);
     return arr.length % 2 ? 1 : 2;
 }
