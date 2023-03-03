947205889ad07509a337ba92a9bb3a191d91623e
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-03-03 18:03:10 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,7 +1,4 @@
 function solution(age) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     let arr = String(age).split('');
-    let answer = '';
-    for(let i of arr) answer += alphabet[i];
-    return answer;
+    return arr.map((i) => 'abcdefghij'[i]).join('')
 }
