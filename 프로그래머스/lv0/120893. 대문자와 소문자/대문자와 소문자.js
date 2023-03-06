1da98076ead50a029583db48fdad317e76a56d98
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-03-06 09:18:45 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,14 +1,3 @@
 function solution(my_string) {
-    let arr = my_string.split('');
-    let str = '';
-    for(let i of arr) {
-        if(i === i.toUpperCase()) {
-            i = i.toLowerCase();
-            str += i;
-        } else {
-            i = i.toUpperCase();
-            str += i;
-        }
-    }
-    return str;
+    return my_string.split('').map((v) => v === v.toUpperCase() ? v.toLowerCase() : v.toUpperCase()).join('');
 }
