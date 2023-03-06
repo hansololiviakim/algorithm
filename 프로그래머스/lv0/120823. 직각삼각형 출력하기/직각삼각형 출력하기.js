893ed6dfe2336871e73c1eb6cfe3067621d8b295
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-03-06 09:21:52 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 42.08 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    const star = Number(input[0]);
+    for(let i = 1; i <= star; i++) {
+        console.log('*'.repeat(i));
+    }
+});
