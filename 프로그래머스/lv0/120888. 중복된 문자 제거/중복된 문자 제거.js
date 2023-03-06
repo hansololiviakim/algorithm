dc0f74436841119f56a3b855c80e9a563258882a
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-03-06 10:26:12 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,7 +1,5 @@
 function solution(my_string) {
     const arr = my_string.split('');
-    const uniqueArr = arr.filter((element, index) => {
-        return arr.indexOf(element) === index;
-    });
-    return uniqueArr.join('');
+    const set = new Set(arr);
+    return [...set].join('');
 }
