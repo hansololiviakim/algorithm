49dd5f8067db3c9e30b07ed9cc82042b7f272e8b
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-03-13 21:29:11 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,9 +1,4 @@
 function solution(array, n) {
-    let count = 0;    
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] === n) {
-            count += 1;
-        }
-    }
-    return count;
+    const answer = array.filter((e) => e === n);
+    return answer.length;
 }
