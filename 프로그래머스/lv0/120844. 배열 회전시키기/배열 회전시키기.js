618e803c9d49b4f45394654dd035c41aa2f2bdb8
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-03-14 01:47:20 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,10 @@
+function solution(numbers, direction) {
+    if(direction === 'right') {
+        numbers.unshift(numbers[numbers.length - 1]);
+        numbers.pop();
+    } else {
+        numbers.push(numbers[0]);
+        numbers.shift();
+    };
+    return numbers;
+}
