4829ea286d4f209548832268e1d0efd26d7e3ea8
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-03-13 21:37:25 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,7 +1,3 @@
 function solution(num_list) {
-    let answer = [];
-    for(let i = num_list.length - 1; i >= 0; i--) {
-        answer.push(num_list[i]);
-    }
-    return answer;
+    return num_list.reverse();
 }
