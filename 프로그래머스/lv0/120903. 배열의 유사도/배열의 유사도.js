09842d318986142665bd9c297ce68c0c0fe14454
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-03-13 21:46:21 +0900

    [level 0] Title: 배열의 유사도, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,3 @@
 function solution(s1, s2) {
-    let answer = 0;
-    for(let i of s1) {
-        for(let j = 0; j < s2.length; j++) {
-            if(i === s2[j]) answer++;
-        }
-    }
-    return answer;
+    return s1.filter(e => s2.includes(e)).length;
 }
