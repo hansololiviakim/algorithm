e3bf400b34a1d891c2e722abdc40cbf9212222d8
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-07 13:46:41 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,4 @@
 function solution(arr) {
-    const index = arr.indexOf(Math.min.apply(Math, arr));  
-    arr.splice(index , 1);
-    if(arr.length === 0) arr.push(-1);
-    return arr;
+    const filterArr = arr.filter(e => e !== Math.min(...arr));
+    return filterArr.length === 0 ? [-1] : filterArr;
 }
