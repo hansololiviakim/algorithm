e512116bb55e44d2a3c8c97c7794102c92bcc4ed
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-07 16:14:59 +0900

    [level 1] Title: 최소직사각형, Time: 8.28 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 38.7 MB, 시간: 2.33 ms
+메모리: 40 MB, 시간: 8.28 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,18 +1,9 @@
 function solution(sizes) {
-    let width = 0;
-    let height = 0;
-    
-    for(let i = 0; i < sizes.length; i++){
-        if(sizes[i][0] < sizes[i][1]){
-            sizes[i].reverse();
-        }
-        if(sizes[i][0] > width){
-            width = sizes[i][0];
-        }
-        if(sizes[i][1] > height){
-            height = sizes[i][1];
-        }
+    const arr = sizes.map(([width, height]) => width < height ? [height, width] : [width, height]);
+    let answer = [0, 0];
+    for(let length of arr) {
+        if(length[0] > answer[0]) answer[0] = length[0];
+        if(length[1] > answer[1]) answer[1] = length[1];
     }
-    
-    return width * height;
+    return answer[0] * answer[1];
 }
