3cfad8d37fabc8a600c48d786fdb62b273b2fab3
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-07 11:01:20 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,4 @@
-function solution(phone_number) {        
-    let answer = ""
-    for(let i = 0; i < phone_number.length - 4; i++) answer += phone_number[i].replace(phone_number[i], '*');
-    return answer + phone_number.substring(phone_number.length - 4, phone_number.length)
+function solution(phone_number) {
+    const str = phone_number.slice(0, -4);
+    return phone_number.replace(str, '*'.repeat(str.length));
 }
