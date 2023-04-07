36fe38ddfc8d07c36c274ce58a752563f369f9ee
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-07 10:27:22 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,4 @@
 function solution(numbers) {
-    let total = 0;
-    let num = 0;
-    
-    for(let i = 0; i <= 9; i++) total += i;
-    for(let i of numbers) num += i;
-    
-    return total - num;
+    const sum = numbers.reduce((acc, cur) => acc += cur);
+    return 45 - sum;
 }
