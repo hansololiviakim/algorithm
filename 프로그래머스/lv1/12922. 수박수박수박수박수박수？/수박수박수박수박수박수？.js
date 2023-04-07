9127566e59c8571202f6bb40cca24a15c527ee28
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-07 11:53:23 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,5 +1,3 @@
 function solution(n) {
-    let answer = '';
-    for(let i = 1; i <= n; i++) i % 2 !== 0 ? answer += '수' : answer += '박'
-    return answer;
+    return '수박'.repeat(n / 2) + (n % 2 ? '수' : '');
 }
