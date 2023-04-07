0dd0ea0d60a548528bb756b1c837a856fe28b9b4
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-07 11:32:44 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,6 +1,6 @@
 function solution(a, b) {
-    const DATE = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
-    let inputDay = new Date(`2016-${a}-${b}`);
-    let getDay = inputDay.getDay();
-    return DATE[getDay]
+    const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    const date = new Date(`2016-${a}-${b}`).getDay();
+    return week[date];
+    
 }
