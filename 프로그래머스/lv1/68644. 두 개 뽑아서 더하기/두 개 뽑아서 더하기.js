f6ebd76e1d9c609503317fc638de882c51e687d9
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-07 16:27:21 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,13 +1,9 @@
 function solution(numbers) {  
-    let sumArr = [];
+    let answer = [];
     for(let i = 0; i < numbers.length; i++) {
-        for(let j = 0; j < numbers.length; j++) {
-            if(i !== j) {
-                sumArr.push(numbers[i] + numbers[j]);
-            }
+        for(let j = i + 1; j < numbers.length; j++) {
+            answer.push(numbers[i] + numbers[j]);
         }
     }
-    const set = new Set(sumArr);
-    const uniqueArr = [...set];
-    return uniqueArr.sort((a, b) => a - b);
+    return [...new Set(answer)].sort((a, b) => a - b);
 }
