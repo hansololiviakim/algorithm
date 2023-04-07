7c7b21dbbf21cf0ee39412feb25e8f300e94d8d3
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-07 10:23:43 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,6 +1,5 @@
 function solution(a, b) {
     let answer = 0;
-    if(a <= b) for(let i = a; i <= b; i++) answer += i;
-    else for(let i = b; i <= a; i++) answer += i;
-    return answer
+    for(let i = Math.min(a, b); i <= Math.max(a, b); i++) answer += i;
+    return answer;
 }
