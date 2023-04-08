3da4b2711606ff49a063e3c0eb627b2023e49af2
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-08 11:11:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,11 +1,8 @@
 function solution(s) {
-    let word = s.split(' ');
-    let wierdword = ""
-    for(let i of word){
-        for(let j = 0; j < i.length; j++) {
-            !(j % 2) ? wierdword += i[j].toUpperCase(j) : wierdword += i[j].toLowerCase(j);
-        }
-        wierdword += ' ';
-    }
-    return wierdword.substring(0, wierdword.length - 1);
+    const arr = s.toUpperCase().split(' ');
+    return arr.map(word => {
+        return word = word.split('').map((spell, idx) => {
+            return idx % 2 ? spell.toLowerCase() : spell.toUpperCase();
+        }).join('');
+    }).join(' ');
 }
