d5f8b10d61cefd57ebda60ee128e1e3ab58be76d
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-10 18:15:12 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,12 +1,6 @@
 function solution(left, right) {
-    let arr = [];
-    for(let i = left; i <= right; i++) arr.push(i);
-    
-    for(let j = 0; j < arr.length; j++) {
-        let sum = 1;
-        for(let k = 1; k <= arr[j] / 2; k++) if(arr[j] % k === 0) sum++;
-        if(sum % 2) arr[j] = arr[j] * -1;
-    }
-
-    return arr.reduce((acc, cur) => acc += cur);
+    let arr = Array.from(Array(right - left + 1), (_, i) => i + left);
+    let answer = 0;
+    arr.map((e, idx) => Number.isInteger(Math.sqrt(e)) ? answer -= e : answer += e);
+    return answer;
 }
