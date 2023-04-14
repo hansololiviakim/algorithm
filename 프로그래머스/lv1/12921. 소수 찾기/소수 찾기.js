107ca1f24c945a7e9b99758957df44dbccc39d26
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-14 22:45:21 +0900

    [level 1] Title: 소수 찾기, Time: 29.25 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,28 +1,7 @@
-// function solution(n) {
-//     const num = Array(n + 1).fill(true);
-//     console.log(num)
-//     let arr = [];
-//     for(let i = 2; i < n; i++) {
-//         if(num[i]) {
-//             arr.push(i);
-//             for(let j = 2; j < n; j++) {
-//                 num[i * j] = false;
-//             }
-//         }
-//     }
-//     console.log(num)
-//     console.log(arr)
-// }
-
 function solution(n) {
-    let result = 0
-    for (let i = 2; i <= n; i++) if (isPrime(i)) result++;
-    return result;
-}
-
-function isPrime(num) {
-    if (num % 2 === 0) return num === 2 ? true : false;
-    const s = parseInt(Math.sqrt(num));
-    for (i = 3; i <= s; i += 2) if (num % i === 0) return false;
-    return true;
+    const arr = Array(n + 1).fill(true).fill(false, 0, 2);
+    for(let i = 2; i * i <= n; i++) {
+        if(arr[i]) for(let j = i * i; j <= n; j += i) arr[j] = false;
+    }
+    return arr.filter(e => e).length;
 }
