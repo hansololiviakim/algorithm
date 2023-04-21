ed19154af623e22d69ae06485e1ae7a3bc98e09c
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-21 13:51:24 +0900

    [unrated] Title: 배열 만들기 6, Time: 4.65 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,15 +1,8 @@
 function solution(arr) {
     let stk = [];
     for(let i = 0; i < arr.length; i++) {
-        if(stk.length === 0) {
-            stk.push(arr[i]);
-            continue;
-        } else if( stk[stk.length - 1] === arr[i] ) {
-            stk.pop();
-        } else {
-            stk.push(arr[i]);
-        }
-        i + 1;
+        if(stk[stk.length - 1] === arr[i]) stk.pop();
+        else stk.push(arr[i]);
     }
     return stk.length === 0 ? [-1] : stk;
 }
