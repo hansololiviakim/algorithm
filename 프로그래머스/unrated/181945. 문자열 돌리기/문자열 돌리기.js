d512c4bff802c8be6f564bce2249e48139e2a35a
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-04-22 03:53:33 +0900

    [unrated] Title: 문자열 돌리기, Time: 42.48 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    for(let i of input[0]) console.log(i);
+});
