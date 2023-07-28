f3a225df816dba30435b5e876ad69a642d0bebd4
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-07-29 00:50:42 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.js
+++ 백준/Bronze/8393. 합/합.js
@@ -1,4 +1,3 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString();
-const arr = Array.from(Array(+input), (_, i) => i + 1);
-console.log(arr.reduce((acc, cur) => (acc += cur), 0));
+console.log((+input * (+input + 1)) / 2);
