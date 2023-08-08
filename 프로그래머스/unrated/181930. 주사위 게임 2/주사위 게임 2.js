8bcf72d63648ffae68bec80354c1bc0beb544d65
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-08 23:58:05 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,9 @@
+const solution = (a, b, c) => {
+    if (a === b && b === c) {
+        return (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3);
+    } else if (a !== b && a !== c && b !== c) {
+        return a + b + c;
+    } else {
+        return (a + b + c) * (a ** 2 + b ** 2 + c ** 2);
+    }
+}
