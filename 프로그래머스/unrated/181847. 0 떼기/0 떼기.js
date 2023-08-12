27a0ed410289f48c1631dab6faa6c2302d2cfab8
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-12 20:33:30 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,4 +1,1 @@
-const solution = (n_str) => {
-    const num = [...n_str].findIndex(e => e !== '0');
-    return n_str.slice(0, num).replace(/0/g, '') + n_str.slice(num);
-}
+const solution = (n_str) => String(+n_str);
