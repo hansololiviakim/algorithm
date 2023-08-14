990335a59934a5996fc5030301f5b6cc88818236
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-15 00:04:24 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,5 +1,5 @@
-function solution(array, n) {
-    const arr = array.sort((a, b) => a - b).map(e => Math.abs(e - n));
-    const idx = arr.indexOf(Math.min(...arr));
-    return array.sort((a, b) => a - b)[idx]
+const solution = (array, n) => {
+    const abs = [...array].map(e => Math.abs(e - n));
+    const idx = abs.map((e, idx) => e === Math.min(...abs) ? array[idx] : 0).filter(e => e !== 0);
+    return Math.min(...idx);
 }
