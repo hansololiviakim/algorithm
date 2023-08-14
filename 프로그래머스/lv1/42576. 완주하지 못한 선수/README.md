b8858ee28350fd62dcb32298576f919fb17af5bc
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-14 23:15:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 30.28 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 57 MB, 시간: 92.48 ms
+메모리: 63.3 MB, 시간: 30.28 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 58.3<br/>효율성: 41.7<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
@@ -68,8 +68,6 @@
 <hr>
 
 <p>※ 공지 - 2023년 01월 25일 테스트케이스가 추가되었습니다.</p>
-
-<p><a href="http://hsin.hr/coci/archive/2014_2015/contest2_tasks.pdf" target="_blank" rel="noopener">출처</a></p>
 
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,12 @@
-function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for(let i = 0; i < participant.length; i++) {
-        if(participant[i] !== completion[i]) {
-            return participant[i];
-        }
+const solution = (p, c) => {
+    const map = new Map();
+    
+    for (let i = 0; i < p.length; i++) {
+        map.set(p[i], (map.get(p[i]) || 0) + 1);
+        map.set(c[i], (map.get(c[i]) || 0) - 1);
+    }
+        
+    for (const [name, value] of map) {
+        if (value > 0) return name;
     }
 }
