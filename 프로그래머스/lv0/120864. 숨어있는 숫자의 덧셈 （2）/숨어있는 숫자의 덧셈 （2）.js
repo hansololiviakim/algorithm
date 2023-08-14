43ed2f75d4d5cbf369c2424ae211d9b42d9ba2b5
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-14 23:21:19 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,6 +1,1 @@
-function solution(my_string) {
-    let arr = my_string.split(/[a-zA-Z]/g);
-    let answer = 0;
-    for(let i of arr) answer += Number(i);
-    return answer;
-}
+const solution = (my_string) => my_string.split(/[a-zA-Z]/g).reduce((acc, cur) => acc += +cur, 0);
