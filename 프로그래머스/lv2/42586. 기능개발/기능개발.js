73935c9d4945b3d732210ef291fe2838752544cc
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-16 00:16:29 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,19 +1,16 @@
 const solution = (progresses, speeds) => {
-    const answer = [];
+    const answer = [0];
     const days = progresses.map((e, idx) => Math.ceil((100 - e) / speeds[idx]));
-    let count = 1;
     let maxDay = days[0];
     
-    for (let i = 1; i < days.length; i++) {
-        if (days[i] <= maxDay) {
-            count++;
+    for (let i = 0, j = 0; i < days.length; i++) {
+        if (maxDay >= days[i]) {
+            answer[j]++;
         } else {
             maxDay = days[i];
-            answer.push(count);
-            count = 1;
+            answer[++j] = 1;
         }
     }
-    answer.push(count);
     
     return answer;
 }
