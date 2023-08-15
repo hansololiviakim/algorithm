32fe6c0412aff676d58eff78314d186a45412387
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-15 12:51:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.17 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,1 +1,7 @@
-const solution = (arr) =>arr.filter((_, idx) => arr[idx] !== arr[idx + 1]);
+const solution = (arr) => {
+    let answer = [];
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) answer.push(arr[i]);
+    }
+    return answer;
+}
