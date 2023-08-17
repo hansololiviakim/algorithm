059080d056e77589c13b01abc609cf603d61cc20
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-17 16:30:35 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,6 +1,8 @@
 const solution = (array, commands) => {
-    return commands.map(e => {
+    let answer = [];
+    commands.map(e => {
         const [i, j, k] = e;
-        return array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+        answer.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]);
     })
+    return answer;
 }
