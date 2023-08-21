3bc22b653efb026d5bae0cd4b29558f7a580cd0a
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-21 11:16:10 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,8 @@
+const solution = (my_string) => {
+    const arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const answer = Array(52).fill(0);
+    
+    for (let i of my_string) answer[arr.indexOf(i)]++;
+    
+    return answer;
+}
