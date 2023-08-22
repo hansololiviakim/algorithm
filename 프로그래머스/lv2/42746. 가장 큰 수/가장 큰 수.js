89fa15992c1b4c23777c361511645680f62a5ee8
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-08-22 22:48:34 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,19 +1,4 @@
 function solution(numbers) {
-    // 모든 숫자가 0인 경우 처리
-    if (numbers.every(num => num === 0)) {
-        return '0';
-    }
-
-    // 숫자를 문자열로 변환하여 비교하기 위한 비교 함수
-    const compare = (a, b) => {
-        const strA = a.toString();
-        const strB = b.toString();
-        return (strB + strA) - (strA + strB);
-    };
-
-    // 주어진 숫자 배열을 비교 함수를 사용하여 정렬
-    numbers.sort(compare);
-    
-    // 정렬된 숫자들을 문자열로 이어붙여서 반환
-    return numbers.join('');
+    const answer = numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join('');
+    return answer[0] === '0' ? '0' : answer;
 }
